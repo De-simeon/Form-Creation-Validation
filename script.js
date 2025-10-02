c1d3d22bb391a712e7c3f26f3906627dc1e7abab
Desimeon <sbdaramola@gmail.com>
2025-10-02 13:16:24 +0100

    Modified script.js

--- script.js
+++ script.js
@@ -5,27 +5,27 @@ document.addEventListener('DOMContentLoaded', function(){
   form.addEventListener('submit', function(event){
     event.preventDefault()
 
-    const userField = document.getElementById('username').value.trim();
-    const emailField = document.getElementById('email').value.trim();
-    const passwordField = document.getElementById('password').value.trim();
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
     let isValid = true;
 
     const messages = [];
 
     // Username Validation Check
-    if(userField.length < 3){
+    if(username.length < 3){
       isValid = false;
       messages.push("username must be more than 3 character long");
     }
 
     // Email Validation Check
-    if(!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailField)){
+    if(!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)){
       isValid = false;
       messages.push("email must include '@' abd '.'");
     }
 
     // Password Validation Check
-    if(passwordField.length < 8){
+    if(password.length < 8){
       isValid = false;
       messages.push("Password must be at least 8 characters long");
     }
